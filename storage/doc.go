b36dde35c35979010dc74eb1295358c724628a3f
evilsocket <evilsocket@gmail.com>
2018-04-21 05:33:48 +0200

    misc: small fix or general refactoring i did not bother commenting

--- storage/doc.go
+++ storage/doc.go
@@ -1,2 +1,3 @@
-// Package storage provides the basic data structures, indexing and persistency.
+// Package storage provides the basic data structures, indexing, persistency and
+// low leve API for oracles and vectors.
 package storage
