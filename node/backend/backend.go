a0547b4e0a9c5231d821d0e7e567a48494c9b7e9
evilsocket <evilsocket@gmail.com>
2019-05-16 19:44:02 +0200

    doc: added some doc to the backend package

--- node/backend/backend.go
+++ node/backend/backend.go
@@ -1,7 +1,7 @@
 package backend
 
 // Vector is an opaque interface to whatever the specific backend implementation
-// will return as object wrapper.
+// will return as an object wrapper/reference.
 type Vector interface{}
 
 type implementation interface {
@@ -15,14 +15,17 @@ type implementation interface {
 // TODO: pick at runtime the best backend available ( CUDA, OpenCL or BLAS32 ).
 var impl = blas{}
 
+// Name returns the name of the current backend.
 func Name() string {
 	return impl.Name()
 }
 
+// Wrap creates an opaque reference to the data, backend specific.
 func Wrap(size int, data []float32) Vector {
 	return impl.Wrap(size, data)
 }
 
+// Dot performs the dot product between a vector and another.
 func Dot(a, b Vector) float64 {
 	return impl.Dot(a, b)
 }
--- node/backend/doc.go
+++ node/backend/doc.go
@@ -1,4 +1,12 @@
 /*
-Package backend provides an abstraction layer to the available BLAS backends.
+Package backend provides an abstraction layer to the available computational backends, currently implemented:
+
+	- naive (naive implementation, no optimizations)
+	- blas32 (gonum blas32 interface
+
+Future:
+
+	- cuda
+	- opencl
 */
 package backend
