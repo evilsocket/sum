170964d14ae2ff039ce5568b5884d5d9aa328b5a
tux-mind <1824153+tux-mind@users.noreply.github.com>
2019-05-20 12:35:11 +0200

    ZMLWR-48: skip creation of duplicate oracles

--- master/oracle_stealer.go
+++ master/oracle_stealer.go
@@ -3,8 +3,8 @@ package master
 import (
 	"context"
 	"fmt"
-	. "github.com/evilsocket/sum/proto"
 	"github.com/evilsocket/islazy/log"
+	. "github.com/evilsocket/sum/proto"
 )
 
 // steal oracles from underlying nodes
@@ -16,14 +16,28 @@ func (ms *Service) stealOracles() {
 
 // steal an oracle form another node
 func (ms *Service) stealOraclesFromNode(n *NodeInfo) {
-	n.RLock()
-	defer n.RUnlock()
+	nOracles := n.Status().Oracles
 
-	for i := uint64(0); i < n.status.Oracles; i++ {
+	for i := uint64(0); i < nOracles; i++ {
 		if err := ms.deployAgentSmith(n, i+1); err != nil {
 			log.Error("Failed to absorb oracle: %v", err)
 		}
 	}
+
+	go n.UpdateStatus()
+}
+
+func (ms *Service) doIHaveThisOracle(oracle *Oracle) bool {
+	ms.cageLock.RLock()
+	defer ms.cageLock.RUnlock()
+
+	for _, raccoon := range ms.raccoons {
+		if raccoon.IsEqualTo(oracle) {
+			return true
+		}
+	}
+
+	return false
 }
 
 // send agent Smith to absorb the oracle
@@ -38,12 +52,15 @@ func (ms *Service) deployAgentSmith(n *NodeInfo, oracleId uint64) error {
 	}
 
 	oracle := resp.Oracle
-	//TODO: check for duplicates.
 
-	if resp1, err := ms.CreateOracle(context.Background(), &Oracle{Code: oracle.Code, Name: oracle.Name}); err != nil || !resp1.Success {
-		return fmt.Errorf("unable to load oracle #%d (%s) from node %d: %v",
-			oracleId, oracle.Name, n.ID, getErrorMessage(err, resp1))
-	} else if resp2, err := n.Client.DeleteOracle(ctx, &ById{Id: oracleId}); err != nil || !resp2.Success {
+	if !ms.doIHaveThisOracle(oracle) {
+		if resp1, err := ms.CreateOracle(context.Background(), &Oracle{Code: oracle.Code, Name: oracle.Name}); err != nil || !resp1.Success {
+			return fmt.Errorf("unable to load oracle #%d (%s) from node %d: %v",
+				oracleId, oracle.Name, n.ID, getErrorMessage(err, resp1))
+		}
+	}
+
+	if resp2, err := n.Client.DeleteOracle(ctx, &ById{Id: oracleId}); err != nil || !resp2.Success {
 		log.Warning("cannot delete oracle #%d (%s) from node %d: %v",
 			oracleId, oracle.Name, n.ID, getErrorMessage(err, resp2))
 	}
